d758e83c61fc296226f0c7c7b9bf6b77b45e221b
Paolo <paolo@MBP-di-Paolo.homenet.telecomitalia.it>
2018-03-18 14:55:11 +0100

    make it better

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+// Dependencies
+
+const express = require('express');
+const socketIO = require('socket.io');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+const http = require('http');
+
+const app = express();
+var server = http.Server(app);
+var io = socketIO(server);
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
